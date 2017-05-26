cbeaddf27b27821485acba3cd281a94ba83b22b1
mmloo <mmloo@ml.com>
2017-05-26 20:50:16 +0800

    第四次提交

---
+++ adad.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Administrator on 2017/5/26 0026.
+ */
+$(function () {
+	$(".adad-nav1>ul>li").on("mouseenter",function () {
+		console.log($(this).index());
+		$(this).addClass("blue").siblings().removeClass("blue");
+		$(".adadad1>div").eq($(this).index()).css("display","flex")
+			.siblings().css("display","none");
+	})
+})
+
+$(function () {
+	$(".adad-nav2>ul>li").on("mouseenter",function () {
+		console.log($(this).index());
+		$(this).addClass("blue").siblings().removeClass("blue");
+		$(".adadad2>div").eq($(this).index()).css("display","flex")
+			.siblings().css("display","none");
+	})
+})
+
+$(function () {
+	$(".adad-nav3>ul>li").on("mouseenter",function () {
+		console.log($(this).index());
+		$(this).addClass("blue").siblings().removeClass("blue");
+		$(".adadad3>div").eq($(this).index()).css("display","flex")
+			.siblings().css("display","none");
+	})
+})
+
+$(function () {
+	$(".adad-nav4>ul>li").on("mouseenter",function () {
+		console.log($(this).index());
+		$(this).addClass("blue").siblings().removeClass("blue");
+		$(".adadad4>div").eq($(this).index()).css("display","flex")
+			.siblings().css("display","none");
+	})
+})
